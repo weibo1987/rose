6e4f8672f8329c877bf453150fd87eac2f55c3cf
weibo.qin <weibo.qin@jrj.com.cn>
2015-01-15 21:38:48 +0800

    init rose

---
+++ src/java/com/jrj/msite/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.jrj.msite.controllers;
+
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.paoding.rose.web.Invocation;
+public class HelloController {
+
+	private  Log log=LogFactory.getLog(this.getClass());
+	public String world(Invocation inv) {
+		log.info("piao guo .....");
+		inv.addModel("now", new Date());
+		return "helloworld";
+	}
+}
